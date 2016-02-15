d113b7ed629a66160f9c3f379586f5fa3ff092f4
Agnieszka Kurek <agniechakurek@gmail.com>
2016-02-15 13:49:26 +0100

    add another  variable  to the function

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -2,4 +2,6 @@ function addDnDHandlers() {
     var coffeeimages = document.getElementsByClassName("productarticlewide");
     
     var shoppingCartDropzone = document.getElementById("shoppingcart");
+    
+    var shopingcart = document.querySelectorAll("#shoppingcart ul")[0];
 }
