803b793f3ca441752f1fc757ce6a33e6765c06a5
Agnieszka Kurek <agniechakurek@gmail.com>
2016-02-18 14:26:47 +0100

    add buttons that support  video player

---
+++ videoplayer.js
@@ -0,0 +1,52 @@
+function initializeVideoPlayerControls(){
+    
+    var video = document.getElementById("videoplayer");
+    
+    function playVideo(evt) {
+        
+        button = evt.target;
+        if (video.paused) {
+            video.play();
+            button.textContent = "Pause";
+        } 
+        else {
+            video.pause();
+            button.textContent = "Play";      
+              }
+    }
+    
+    function seek(numberOfSecond) {
+        try{
+            if (numberOfSecond == 0) {
+                video.currentTime = numberOfSecond;
+            }
+            else {
+                video.currentTime  += numberOfSecond;
+            }
+        } catch (err) {
+            displayError("Something went wrong...");
+        }
+    }
+    
+    document.getElementById("playButton").addEventListener("click", playVideo, false);
+    
+    document.getElementById("backButton").addEventListener("click", function () {
+        seek(-5);
+    }, false);
+    
+    document.getElementById("slowerButton").addEventListener("click", function () {
+        video.playbackRate -= .25;
+    }, false);
+    document.getElementById("fasterButton").addEventListener("click", function () {
+        video.playbackRate += .25;
+    }, false);
+    document.getElementById("muteButton").addEventListener("click", function (s) {
+        video.playbackRate -= .25;
+        if (video.muted) {
+            video.muted = false;
+        }
+        else{
+            video.muted = true;
+        }
+    }, false);
+}
