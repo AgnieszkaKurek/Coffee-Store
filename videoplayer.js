dfb9778ffd6d2403d9537debf5f2881487f14118
Agnieszka Kurek <agniechakurek@gmail.com>
2016-02-19 13:08:20 +0100

    more changes

--- videoplayer.js
+++ videoplayer.js
@@ -1,4 +1,4 @@
-function initializeVideoPlayerControls(){
+function initializeVideoPlayerControls() {
     
     var video = document.getElementById("videoplayer");
     
@@ -15,29 +15,33 @@ function initializeVideoPlayerControls(){
               }
     }
     
-    function seek(numberOfSecond) {
+    function seek(numberOfSeconds) { 
         try{
-            if (numberOfSecond == 0) {
-                video.currentTime = numberOfSecond;
+            if (numberOfSeconds == 0) {
+                video.currentTime = numberOfSeconds;
             }
             else {
-                video.currentTime  += numberOfSecond;
+                video.currentTime  += numberOfSeconds;
             }
         } catch (err) {
             displayError("Something went wrong...");
         }
     }
     
-    function displayError(message){
-        
-    }
+   function displayError(message){
+       
+   }
+   
     
     document.getElementById("playButton").addEventListener("click", playVideo, false);
     
     document.getElementById("backButton").addEventListener("click", function () {
         seek(-5);
     }, false);
-    
+     document.getElementById("forwardButton").addEventListener("click", function () {
+        seek(5);
+    }, false);
+
     document.getElementById("slowerButton").addEventListener("click", function () {
         video.playbackRate -= .25;
     }, false);
@@ -54,3 +58,12 @@ function initializeVideoPlayerControls(){
         }
     }, false);
 }
+
+ video.addEventListener("error", function (err) {
+        errMessage(err);
+        
+    }, true);
+
+    function displayError(error) {
+        document.getElementById("errorDiv").textContent = error;
+    }
